187bc990c98a72f4f894ee8919f8befb03177f64
babyazalea <usun12@hotmail.com>
2021-03-17 23:16:15 +0900

    using setup func & ref

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,23 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  data() {
-    return {
-      userName: 'Maximilian',
-    };
-  },
+  setup() {
+    const uName = ref('babyazalea');
+
+    setTimeout(function() {
+      uName.value = 'ty';
+    }, 2000);
+
+    return { userName: uName };
+  }
+  // data() {
+  //   return {
+  //     userName: 'babyazalea',
+  //   };
+  // },
 };
 </script>
 
